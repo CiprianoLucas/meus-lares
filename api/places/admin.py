8a67dc00afddb53fd8d9835c2b950d435a88998d
Lucas Cipriano <lucas.cipriano@datafrete.com.br>
2024-11-01 19:59:58 -0300

    feat: serializer cidades

--- api/places/admin.py
+++ api/places/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Place
 
 class PlacesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'number', 'street', 'city', 'state', 'is_active', 'created_at', 'representative')
-    list_filter = ('id', 'is_active', 'city', 'state', 'representative')
-    search_fields = ('id', 'name', 'number', 'street', 'city', 'state', 'representative__username')
-    fields = ('representative', 'residents', 'unions', 'name', 'number', 'street', 'city', 'state', 'is_active', 'profile_photo')
+    list_display = ('id', 'name', 'number', 'street', 'city', 'city__state', 'is_active', 'created_at', 'representative')
+    list_filter = ('id', 'is_active', 'city','city__state', 'representative')
+    search_fields = ('id', 'name', 'number', 'street', 'city', 'representative__username')
+    fields = ('representative', 'residents', 'unions', 'name', 'number', 'street', 'city', 'is_active', 'profile_photo')
     list_per_page = 20
     verbose_name = "Local"
     verbose_name_plural = "Locais"
--- api/places/serializers.py
+++ api/places/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Place
+from .models import Place, City
 class PlacesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
-        fields = ['id', 'name', 'number', 'street', 'city', 'state']
+        fields = ['id', 'name', 'number', 'street', 'city']
         
     def delete(self, instance, validated_data=None):
         instance.is_active = False
@@ -14,3 +14,19 @@ class UserPlaceSerializer(serializers.Serializer):
     id = serializers.UUIDField(read_only=True)
     username = serializers.CharField(read_only=True)
     email = serializers.EmailField()
+    
+class CitySerializer(serializers.ModelSerializer):
+    state_acronym = serializers.SerializerMethodField()
+    class Meta:
+        model = City
+        fields = ["id", "name", "state_acronym"]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "name": {"read_only": True},
+            "state_acronym": {"read_only": True},
+        }
+    
+    def get_state_acronym(self, obj):
+        if obj.place:
+            return obj.state.acronym
+        return None
